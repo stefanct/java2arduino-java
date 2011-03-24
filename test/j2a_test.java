9cd3f567686bf53857455711c0f678f8388c78e8
Stefan Tauner <stefan.tauner@student.tuwien.ac.at>
2011-03-24 07:31:03 +0100

    - add a test class that uses a2jEcho and a2jEchoMany to check that most of the basic stuff works.

---
+++ test/j2a_test.java
@@ -0,0 +1,138 @@
+import j2arduino.ArduinoGateway;
+import j2arduino.devices.Arduino;
+
+import javax.usb.UsbException;
+import java.io.IOException;
+import java.util.*;
+
+public class j2a_test{
+
+static final String lineSeparator = System.getProperty("line.separator");
+
+private j2a_test(){
+}
+
+static public void main(String[] args) throws IOException{
+	ArduinoGateway ag = ArduinoGateway.getInstance();
+	try{
+		final Iterator<Arduino> iterator = ag.getAvailableArduinos(true).iterator();
+		if(!iterator.hasNext()){
+			System.err.println("No remote device found, bailing.");
+			return;
+		}
+		Arduino a = iterator.next();
+		try{
+			a.connect(null);
+			int[] ints = stresstestEcho(a);
+			System.out.format("stresstest a2jEcho %5s (%dB of %dB came back correctly)%s",
+			                  ints[1] == 0 ? "OK" : "FAILED",
+			                  ints[0],
+			                  ints[0] + ints[1],
+			                  lineSeparator);
+			ints = stresstestEchoMany(a);
+			System.out.format("stresstest a2jEchoMany %5s (%dB of %dB came back correctly)%s",
+			                  ints[1] == 0 ? "OK" : "FAILED",
+			                  ints[0],
+			                  ints[0] + ints[1],
+			                  lineSeparator);
+
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		a.disconnect();
+	} catch(InterruptedException e){
+		e.printStackTrace();
+	} catch(UsbException e){
+		e.printStackTrace();
+	} catch(IOException e){
+		e.printStackTrace();
+	}
+}
+
+private static int[] stresstestEcho(Arduino a) throws InterruptedException{
+	int i = 100;
+	int ok = 0;
+	int failed = 0;
+	Random rand = new Random(0);
+	while(i-- > 0){
+		int len = rand.nextInt(Arduino.A2J_MAX_PAYLOAD + 1);
+		byte[] payload = new byte[len];
+		rand.nextBytes(payload);
+
+		System.err.format("sending normal payload (%3dB): %s%s", len, toHexString(payload), lineSeparator);
+		try{
+			final byte[] ans = a.sendSyncByName("a2jEcho", payload).msg;
+			System.err.format("received               (%3dB): %s%s", ans.length, toHexString(ans), lineSeparator);
+			if(!Arrays.equals(ans, payload)){
+				System.err.println("FAILED");
+				int payloadLength = payload.length;
+				int j = 0;
+				for(; j < payloadLength; j++){
+					if(ans[j] != payload[j])
+						break;
+				}
+				ok += j;
+				failed += (payloadLength - j);
+			} else{
+				System.err.println("OK");
+				ok += ans.length;
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	return new int[] {ok, failed};
+}
+
+private static int[] stresstestEchoMany(Arduino a) throws InterruptedException{
+	int i = 100;
+	int ok = 0;
+	int failed = 0;
+	Random rand = new Random(0);
+	while(i-- > 0){
+		int len = rand.nextInt(Arduino.A2J_MAX_PAYLOAD * 10);
+		byte[] payload = new byte[len];
+		rand.nextBytes(payload);
+		System.err.printf("sending long payload (%5dB): %s%s", len, toHexString(payload), lineSeparator);
+		try{
+			final byte[] ans = a.sendLongByName("a2jEchoMany", payload).msg;
+			System.err.format("received             (%5dB): %s%s", ans.length, toHexString(ans), lineSeparator);
+			if(!Arrays.equals(ans, payload)){
+				int payloadLength = payload.length;
+				int j = 0;
+				for(; j < payloadLength; j++){
+					if(ans[j] != payload[j])
+						break;
+				}
+				ok += j;
+				failed += (payloadLength - j);
+			} else{
+				System.err.println("OK");
+				ok += ans.length;
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	return new int[] {ok, failed};
+}
+
+private static char[] hexChar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+/**
+ Fast convert a byte array to a hex string with possible leading zero.
+
+ @param b array of bytes to convert to string
+ @return hex representation, two chars per byte. */
+public static String toHexString(byte[] b){
+	StringBuilder sb = new StringBuilder(b.length<<1);
+	for(byte aB : b){
+		// look up high nibble char
+		sb.append(hexChar[(aB&0xf0)>>>4]);
+
+		// look up low nibble char
+		sb.append(hexChar[aB&0x0f]);
+	}
+	return sb.toString();
+}
+}
